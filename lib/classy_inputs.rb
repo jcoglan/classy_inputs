1de88a089f4b29a3b997569afa8a9dfc1398efd9
S. Brent Faulkner <brentf@unwwwired.net>
2008-09-10 01:37:24 -0400

    receiving symbolized keys for some tag options (e.g. submit_to_remote), so stringify for consistency

--- lib/classy_inputs.rb
+++ lib/classy_inputs.rb
@@ -2,6 +2,7 @@ module ClassyInputs
   def tag_with_input_class_name(*args)
     name, options = args[0..1]
     if name.to_s =~ /^input$/
+      options.stringify_keys!
       classes = (options['class'] || '').split(/\s+/)
       type = options['type'] || 'text'
       classes << type unless classes.include?(type)
